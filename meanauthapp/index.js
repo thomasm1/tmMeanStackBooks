65f3bca691d7b2fc9a7afaeddb2dd43425b39545
Thomas M Maestas <tmilton.maestas@gmail.com>
2017-08-03 09:18:59 -0600

    restart on authentication app

---
+++ meanauthapp/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('bodyParser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hola, invalid endpoint, Compadre');
+});
+
+app.listen(port, () => {
+    console.log('Server started on port '+ port);
+});
